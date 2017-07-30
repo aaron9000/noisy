d0cda53e29f17e717dd99d8767c88ad9d897258c
Aaron Geisler <aaron.geisler.sloth@gmail.com>
2017-07-29 19:44:59 -0700

    works with binary

--- js/lib/app.js
+++ js/lib/app.js
@@ -2,24 +2,26 @@ import R from 'ramda';
 import Promise from 'bluebird';
 import {configIsValid} from './config';
 
-const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 
 export function handleConfig(config:Object):Promise {
-    // console.log("debug config", config);
     if (!configIsValid(config)) return Promise.reject(new Error(''));
-    
-
+    const cmd = './bin/' + config.command;
     return new Promise((resolve, reject) => {
-        const ls = spawn('ls', ['-lh', '/usr']);
-        ls.stdout.on('data', data => {
-            console.log(`stdout: ${data}`);
-        });
-        ls.stderr.on('data', data => {
-            console.log(`stderr: ${data}`);
-        });
-        ls.on('close', code => {
-            console.log(`child process exited with code ${code}`);
-            resolve('done');
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject(error);
+            }else{
+                console.log(stdout);
+                if (stderr){
+                    console.log(`lib error: ${stderr}`);
+                }else{
+                    console.log('success');
+                    resolve({success: true});
+                }
+            }
+
         });
     });
 }
